ce6ad1b015f26316ac4b4742abd3ed552b54ab69
stappost <emanuele.scarpantoni@gmail.com>
2023-11-17 14:47:07 +0100

    add var password

--- js/script.js
+++ js/script.js
@@ -4,4 +4,6 @@ let nome = prompt("inserisci il tuo nome");
 
 let cognome = prompt ("inserisci il tuo cognome");
 
-let colore_preferito = prompt ("inserisci il tuo colore preferito")
+let colore_preferito = prompt ("inserisci il tuo colore preferito");
+// CREAZIONE VARIABILE PASSWORD 
+let password = `${nome}${cognome}${colore_preferito}23`;
